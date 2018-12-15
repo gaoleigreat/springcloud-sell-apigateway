e9957aa839a685259c19355e374b57fdc2c2436e
xiaodao <513684652@qq.com>
2018-12-15 19:15:07 +0800

    apigateway初始化

---
+++ src/main/java/com/xiaodao/apigateway/exception/RatelimitException.java
@@ -0,0 +1,11 @@
+package com.xiaodao.apigateway.exception;
+
+/**
+ * @ClassName : RatelimitException
+ * @Description : TODO
+ * @Author : xiaodao
+ * @Date : 2018/12/9  19:25
+ * @Version : 1.0
+ **/
+public class RatelimitException extends RuntimeException {
+}
---
+++ src/main/java/com/xiaodao/apigateway/filter/RateLimitFilter.java
@@ -0,0 +1,42 @@
+package com.xiaodao.apigateway.filter;
+
+import com.google.common.util.concurrent.RateLimiter;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.exception.ZuulException;
+import com.xiaodao.apigateway.exception.RatelimitException;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.SERVLET_DETECTION_FILTER_ORDER;
+
+/**
+ * @ClassName : RateFilter
+ * @Description : TODO
+ * @Author : xiaodao
+ * @Date : 2018/12/9  19:17
+ * @Version : 1.0
+ **/
+public class RateLimitFilter extends ZuulFilter {
+    private static final RateLimiter RATE_LIMITER = RateLimiter.create(100);
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return SERVLET_DETECTION_FILTER_ORDER -1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        if (!RATE_LIMITER.tryAcquire()){
+            throw new RatelimitException();
+        }
+        return null;
+    }
+}
